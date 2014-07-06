097feffe8eddf8a2c0765d28c55011f7c062a3e3
Nate McGuire <nate.mcguire@gmail.com>
2014-07-06 14:39:43 -0500

    changel inks again

--- wp-content/themes/virtue/templates/home/mock-flex.php
+++ wp-content/themes/virtue/templates/home/mock-flex.php
@@ -8,7 +8,7 @@
                         </a>
                       </li>
                       <li> 
-                        <a href="/buy-hammocks/" target="_blank" >
+                        <a href="/buy-hammocks/">
                           <img src="<?php echo get_template_directory_uri(); ?>/assets/img/slide_02.png" width="1170px" height="450px" alt="Example Slider 02"/>
                         </a>
                       </li>
